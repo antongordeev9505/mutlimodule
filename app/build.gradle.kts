252156299f959692aaa598f3ace66f9739364d27
Anton Gordeev <antongordeev9505@gmail.com>
2023-08-02 20:59:27 +0300

    Added dagger, added kapt, increased java version to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -27,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -47,4 +48,8 @@ dependencies {
     // Test
     testImplementation(Libraries.junit)
     androidTestImplementation(Libraries.junitTest)
+
+    // DI
+    implementation(Libraries.implementationDagger)
+    kapt(Libraries.kaptDagger)
 }
