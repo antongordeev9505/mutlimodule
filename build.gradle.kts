70b59447ba1ad5b559fb1698580dc722f857530b
Anton Gordeev <antongordeev9505@gmail.com>
2023-08-02 20:02:38 +0300

    Transformed project build.gradle to kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("com.android.application") version Versions.classpathGradleVersion apply false
+    id("com.android.library") version Versions.classpathGradleVersion apply false
+    kotlin("android") version Versions.kotlinVersion apply false
+    kotlin("jvm") version Versions.kotlinVersion apply false
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,22 @@
+object Versions {
+
+    // Gradle Plugin
+    const val classpathGradleVersion = "8.0.2"
+
+    // Core
+    const val kotlinVersion = "1.8.20"
+//    const val appCompatVersion = "1.6.1"
+//    const val coreKtxVersion = "1.7.0"
+//
+//    // Design
+//    const val constraintLayoutVersion = "2.1.4"
+//    const val materialVersion = "1.9.0"
+//
+//    // Test
+//    const val junitVersion = "4.13.2"
+//    const val junitTestVersion = "1.1.5"
+//    const val espressoCoreVersion = "3.5.1"
+//
+//    // Dagger
+//    const val dagger = "2.46"
+}
